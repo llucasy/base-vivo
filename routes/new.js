8fec55ab5fdbebf833a757047d35278e438555de
llucasy <45694838+llucasy@users.noreply.github.com>
2020-04-24 09:57:53 -0300

    restoUploading

---
+++ routes/new.js
@@ -0,0 +1,48 @@
+var express = require('express'); 
+var router = express.Router();
+
+router.get('/', function(req, res, next) {  
+    res.render('new', { title: 'Novo Cadastro', customer: {_id: '', nome: '', idade:0, uf: ''} }); 
+});
+
+/* POST new page. */
+// router.post('/', function(req, res, next) {
+//     const nome = req.body.nome;
+//     const idade = parseInt(req.body.idade);
+//     const uf = req.body.uf;
+//     global.db.insert({nome, idade, uf}, (err, result) => {
+//       if(err) {return console.log(err); }
+//       res.redirect('/');
+//     })
+//   });
+
+/* Alterar **/
+router.get('/:id', function(req, res, next) {
+    var id = req.params.id;
+    global.db.findOne(id, (err, customer) => {
+        if(err) { return console.log(err); }
+        res.render('new', {title: 'Editar Cadastro', customer});
+    })
+});
+
+/* POST new page. */
+router.post('/', function(req, res, next) {
+    var id = req.body.id;
+    var nome = req.body.nome;
+    var idade = parseInt(req.body.idade);
+    var uf = req.body.uf;
+
+    if(id){
+        global.db.updateOne(id, {nome, idade, uf}, (err, result) => {
+            if(err) { return console.log(err); }
+            res.redirect('/');
+        })
+     }else {
+         global.db.insert({nome, idade, uf}, (err, result) => {
+             if(err) { return console.log(err); }
+             res.redirect('/');
+         })
+     }
+});
+
+module.exports = router;
