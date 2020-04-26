24fc3794077d07463a21ebbf50bd8ca65ed13493
llucasy <45694838+llucasy@users.noreply.github.com>
2020-04-26 18:43:58 -0300

    Contagem de registros no rodapé e adicionado filtros
    Colocado a contagem de registros no rodapé no front, faltando a parte do back. E adicionado filtros Razão Social e Plano.

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,10 +1,19 @@
-const btnPesquisa = document.querySelector('#btnPesquisa')
+const btnPesquisa = document.querySelector("#btnPesquisa");
 
-btnPesquisa.addEventListener('click', () => {
-
-  const cmpPesquisa = document.querySelector('#pesquisa').value
+btnPesquisa.addEventListener("click", () => {
+  const cmpPesquisa = document.querySelector("#pesquisa").value;
 
   if (isNaN(cmpPesquisa)) {
-    return alert('Oi, você colocou letra(as) no campo de busca, não posso aceitar :/ favor corrigir :)')
-  } 
-})
+    return alert(
+      "Oi, você colocou letra(as) no campo de busca, não posso aceitar :/ favor corrigir :)"
+    );
+  }
+});
+
+const contRegistro = document.querySelectorAll(".contRegistro");
+const tfoot = document.querySelector('tfoot tr td').innerHTML;
+let rodape = document.querySelector('tfoot tr td');
+
+rodape.innerHTML = `Exibindo ${contRegistro.length} registros de -  ${tfoot}`;
+
+console.log(contRegistro.length);
