dd2f8c028328dda2aaaa46ae02dc8b725160bcb9
llucasy <45694838+llucasy@users.noreply.github.com>
2020-04-29 19:40:42 -0300

    Colocado todos filtros para funcionar e alterado table-layout
    Retirado sem querer uma dependência e recolocada a mesma.
    
    Defino  table-layout: auto;

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  let pesq = {};
-  let local = {};
+  
+  let pesq = {}
+  let local = {}
+  let razao = {}
+  let plano = {}
+  let status = {}
+
   if (req.query.pesqCamp) {
     let re = new RegExp(`\\b${req.query.pesqCamp}`);
     pesq = { linha: { $regex: re } };
@@ -14,7 +19,19 @@ router.get("/", function(req, res, next) {
     local = { local: req.query.local };
   }
 
-  let item = Object.assign(pesq, local);
+  if (req.query.razao) {
+    razao = { razao: req.query.razao };
+  }
+
+  if (req.query.plano) {
+    plano = { plano: req.query.plano };
+  }
+
+  if (req.query.status) {
+    status = { status: req.query.status };
+  }
+
+  let item = Object.assign(pesq, local, razao, plano, status);
 
   global.db.findAll(item, (e, docs) => {
     if (e) {
